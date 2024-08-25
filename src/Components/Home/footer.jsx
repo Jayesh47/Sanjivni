daa496dca1965d9c6d00efddfdb67f5af0bf37a6
jayesh47 <jayeshmalviya47@gmail.com>
2024-08-25 21:51:37 +0530

    Sanjivni is final

--- src/Components/Home/footer.jsx
+++ src/Components/Home/footer.jsx
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer () {
     return (
         <footer className="footer">
-            <h6>Project developed by Jayesh Lohar, Jay Kumar Pandiya, Kapil Sharma & Mahesh Menariya.</h6>
+            <h6>Project developed by Jayesh Lohar</h6>
         </footer>
     )
 }
